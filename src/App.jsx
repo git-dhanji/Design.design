a51c9e3359c1a873291f40ccf8a643439e9b4a7d
Dhanji Kumar <kdhanji183@gmail.com>
2024-05-11 18:18:59 +0530

    Navbar completed , Add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from "./components/index";
+
 export default function App() {
   return (
-    <div className="w-full h-screen bg-zinc-900">
-      Hello Design.design
+    <div className="w-full h-screen bg-zinc-900 text-white">
+      <Navbar />
     </div>
-  )
+  );
 }
