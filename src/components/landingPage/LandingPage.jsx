d264d5d673c042a0c70fbb932bac23d490d0fb09
Dhanji Kumar <kdhanji183@gmail.com>
2024-05-16 17:13:43 +0530

    landing page marker section is completed

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -1,12 +1,17 @@
-
 export default function LandingPage() {
   return (
-    <div className="w-full h-screen bg-zinc-900">
-      <div className="textstrcture">
-        <div className="masker">
-            <h1>We Create</h1>
-        </div>
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstrcture mt-40 px-20">
+        {["We Create", "Eye Opening", "Persentations"].map((elem, index) => (
+          <div className="masker" key={index}>
+            <h1 className=" uppercase text-8xl font-bold font-FoundersGrotesk  tracking-[-0.4rem]  leading-[6vw]">
+              {elem}
+            </h1>
+          </div>
+        ))}
       </div>
+
+      <div className=" border-t-[1px] border-zinc-700 mt-32"></div>
     </div>
-  )
+  );
 }
