243de8ac8ea78055b2ce5bd251164cfa758ef25b
Dhanji Kumar <kdhanji183@gmail.com>
2024-05-17 14:57:11 +0530

    structure is completed

--- src/components/marquee/Marquee.jsx
+++ src/components/marquee/Marquee.jsx
@@ -1,14 +1,26 @@
+import { motion } from "framer-motion";
+
 export default function Marquee() {
   return (
     <div className="bg-zinc-900">
-      <div className="marquee bg-[#004D43] overflow-hidden w-full h-[50vh] flex items-center rounded-t-lg">
-        <div className="text  text-nowrap  border-t-[2px] border-b-[2px] border-zinc-500 flex gap-4 tra">
+      <div className="marquee bg-[#004D43] overflow-hidden w-full h-[60vh] flex items-center rounded-t-lg">
+        <motion.div
+          initial={{ x: "-20%" }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 15 }}
+          className="text  text-nowrap  border-t-[2px] border-b-[2px] border-zinc-500 flex gap-4 "
+        >
           {["we are devin", "we are devi", "we are devin"].map(
             (elem, index) => (
-              <h1 key={index} className="text-[27vw] pb-9 uppercase leading-[21vw] font-['FoundersGrotesk']">{elem}</h1>
+              <motion.h1
+                key={index}
+                className="text-[27vw] pb-9 uppercase leading-[21vw] font-['FoundersGrotesk']"
+              >
+                {elem}
+              </motion.h1>
             )
           )}
-        </div>
+        </motion.div>
       </div>
     </div>
   );
