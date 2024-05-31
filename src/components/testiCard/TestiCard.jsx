48a92e3d42f69599f11bf465ba47d3e2bbae8a78
Dhanji Kumar <kdhanji183@gmail.com>
2024-05-31 12:18:15 +0530

    card responsive is completed

--- src/components/testiCard/TestiCard.jsx
+++ src/components/testiCard/TestiCard.jsx
@@ -1,28 +1,29 @@
 import Button from "../button/Button";
-import jpeg from '../../assets/jpeg.png'
+import jpeg from '../../assets/jpeg.png';
+
 export default function TestiCard() {
   return (
     <div id="insight">
-      <div className="h-[90vh]  w-full px-20 relative">
-        <div className="grid lg:grid-cols-4 lg:row-span-1 relative h-full place-items-center gap-x-8">
-          <div className="bg-[#004D43] h-[50%] relative w-full rounded-lg col-span-2">
-            <img src={jpeg} alt="" className="bg-cover h-full w-full"/>
+      <div className="h-screen lg:h-[90vh] w-full px-5 py-4 lg:px-20 relative">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:gap-2 h-full place-content-center">
+          <div className="bg-[#004D43] lg:col-span-2 h-[50vh] relative rounded-lg">
+            <img src={jpeg} alt="" className="object-cover h-full w-full" />
             <Button
               text="2023-2034"
               variant="border"
               className="absolute bottom-5 left-5"
             />
           </div>
-          <div className="bg-[#212121] relative h-[50%] w-full rounded-lg cursor-pointer">
+          <div className="bg-[#212121] col-span-2 md:col-auto h-[25vh] lg:h-[50vh] relative rounded-lg cursor-pointer">
             <Button
-              text="rating 5.0 on clutch"
+              text="Rating 5.0 on Clutch"
               variant="border"
               className="absolute bottom-5 left-5 uppercase tracking-wider"
             />
           </div>
-          <div className="bg-[#212121] relative h-[50%] w-full rounded-lg cursor-pointer">
+          <div className="bg-[#212121] col-span-2 md:col-auto h-[25vh] lg:h-[50vh] relative rounded-lg cursor-pointer">
             <Button
-              text="bussiness bootcamp alumani"
+              text="Business Bootcamp Alumni"
               variant="border"
               className="absolute bottom-5 left-5 uppercase tracking-tight"
             />
